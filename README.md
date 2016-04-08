c8a98d0356116057f860ff0d9ade4782791cca85
fenwick <dfharwell@gmail.com>
2016-04-08 01:15:25 +0000

    email filter only

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodebb-email-domain-filter
-an email domain filter for nodebb - only let users with an @gmail.com email address register, and filters out bad words in posts
+An email domain filter for nodebb - only lets users with an @gmail.com email address register.  
+
+You can change the email domain by setting the environment variable `EMAIL_DOMAIN_FILTER` to something else (like `@yahoo.com` etc.)
+
+Tested with nodebb 0.8.x
--- email-domain-filter.js
+++ email-domain-filter.js
@@ -1,80 +1,29 @@
-var emailDomain = '@gmail.com';
-
-var profanityUtil = require('profanity-util');
-var profanityList = null;//override with array for specific strings
+const emailDomain = process.env.EMAIL_DOMAIN_FILTER||'@gmail.com';
 
 var Plugin = {
-        filterEmail: function(regData,next) {
-            //console.log('next:',next);
-            if(regData && regData.userData && regData.userData.email){
-                //console.log('got email',regData.userData.email);
-                if (regData.userData.email.toString().toLowerCase().indexOf(emailDomain) >= 0){
-                 return next(null,regData);  
-                }
+    filterEmailRegister: function(regData,next) {
+        if(regData && regData.userData && regData.userData.email){
+            if ( emailDomainCheck(regData.userData.email) ){
+             return next(null,regData);  
             }
-            return next( new Error('Bad email address: you must register with a '+emailDomain+' email address.') );
-        },
-        profanityHook:function(data,next){
-            //console.log(data);
-            if (!data){
-                return next(null,data);//no data no profanity lol
-            }
-            //check content
-            if (data.content){
-                if(callbackOnProfanity(data.content,next,data)){
-                    return;
-                };
-            }
-            //check topic title
-            if (data.topic && data.topic.title){
-                if(callbackOnProfanity(data.topic.title,next,data)){
-                    return;
-                }
-            }
-            //all ok I guess
-            return next(null,data);
         }
-    };
-    
-    /**
-     * checkProfanity
-     *  returns a list of profane words in the string
-     * 
-     * @param s: the String to search for profanity in
-     * @returns: the array of bad words, or false if no profanity
-     */
-     
-    function checkProfanity(s){
-        
-        var list;
-        if(profanityList){
-            list = profanityUtil.check(s, profanityList);
-        }else{
-            list =  profanityUtil.check(s);
+        return next( new Error('Bad email address: you must register with a "'+emailDomain+'" email address.') );
+    },
+    filterEmailUpdate: function(data,next){
+        if(data && data.email){
+            if ( emailDomainCheck(data.email) ){
+             return next(null,data);  
+            }
         }
-         if (list.length > 0){
-                return list;
-            }else{
-                return false;
-        }
+        return next( new Error('Bad email address: email addresses must end with "'+emailDomain+'" .') );
     }
+}
+
+module.exports = Plugin;
     
-    /**
-     * callbackOnProfanity
-     *  immediately call cb if s contains profanity, with an error message describing which words were not accepted
-     * 
-     * @param s: the String to search for profanity in
-     * @returns: true if profanity was found and cb was called, false if not
-     */
-    function callbackOnProfanity(s,cb,data){
-        var list = checkProfanity(s);
-        if(list){
-            //console.log('found the following bad words: '+list.toString());
-            cb(new Error('Your message contains the following restricted word(s): ' + list.toString() ),data);
-            return true;
-        }else{
-            return false;
-        }
+function emailDomainCheck(e){
+    if (e.toString().toLowerCase().indexOf(emailDomain) >= 0){
+        return true;
     }
-    
-    module.exports = Plugin;
+    return false;
+}
